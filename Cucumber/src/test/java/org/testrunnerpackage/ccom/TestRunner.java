1f4816f9ae8cb4d2e056a9f376ab1c4ff37766da
sasikumar02 <sasikrishna97@gmail.com>
2021-05-11 19:57:18 +0530

    first commit

---
+++ Cucumber/src/test/java/org/testrunnerpackage/ccom/TestRunner.java
@@ -0,0 +1,18 @@
+package org.testrunnerpackage.ccom;
+
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = { "src\\test\\resources" }, glue = "org.stepdefinition",dryRun = false,monochrome = true,
+plugin= {"pretty","json:src\\test\\resources\\Reports\\fb.json"},tags = {"@sanity"})
+
+public class TestRunner {
+	@AfterClass
+	public static void jvmReportGeneration() {
+		JvmReport.generateJvmReport(System.getProperty("user.dir")+"\\src\\test\\resources\\Reports\\fb.json");
+	}
+}
